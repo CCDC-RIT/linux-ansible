de6b786b6ec8fd7099cff8067939c87acbca3ebe
Tri Tran <tritran@Tris-MacBook-Air-3.local>
2025-01-15 19:19:13 -0500

    tell curl to stop looking for a cert

--- palo_alto_test.sh
+++ palo_alto_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 PALO_ALTO="172.20.254.85"
-API_KEY=$(curl -H "Content-Type: application/x-www-form-urlencoded" -X POST https://$PALO_ALTO/firewall/api/?type=keygen -d "user=$USER&password=$PASSWORD")
+API_KEY=$(curl -k -H "Content-Type: application/x-www-form-urlencoded" -X POST https://firewall/api/?type=keygen -d "user=$USER&password=$PASSWORD")
