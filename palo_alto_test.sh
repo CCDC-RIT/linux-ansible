6fad6e4ba9d1ba2885ade0157f04ed2082ee176e
Tri Tran <chemandtech874@gmail.com>
2025-01-15 19:41:10 -0500

    processed api key secretly

--- palo_alto_test.sh
+++ palo_alto_test.sh
@@ -9,5 +9,6 @@ read -s -p "Enter username: " USER
 PASSWORD=""
 read -s -p "Enter password: " PASSWORD
 
-API_KEY=$(curl -k -H "Content-Type: application/x-www-form-urlencoded" -X POST https://$FIREWALL_IP/api/?type=keygen -d "user=$USER&password=$PASSWORD")
+RAW=$(curl -k -H "Content-Type: application/x-www-form-urlencoded" -X POST https://$FIREWALL_IP/api/?type=keygen -d "user=$USER&password=$PASSWORD")
+API_KEY=$(awk -v str="$RAW" 'BEGIN { split(str, parts, "<key>|</key>"); print parts[2] }')
 echo $API_KEY
