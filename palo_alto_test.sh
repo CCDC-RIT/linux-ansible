8c2adc16001ef196f825d5429353c214bdb3677a
Tri Tran <tritran@Tris-MacBook-Air-3.local>
2025-01-15 19:32:17 -0500

    changed to prompts instead of env vars

--- palo_alto_test.sh
+++ palo_alto_test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-PALO_ALTO="172.20.254.85"
-API_KEY=$(curl -k -H "Content-Type: application/x-www-form-urlencoded" -X POST https://firewall/api/?type=keygen -d "user=$USER&password=$PASSWORD")
+FIREWALL_IP=""
+read -s -p "Enter firewall IP: " FIREWALL_IP
+
+USER=""
+read -s -p "Enter username: " USER
+
+PASSWORD=""
+read -s -p "Enter password: " PASSWORD
+
+API_KEY=$(curl -k -H "Content-Type: application/x-www-form-urlencoded" -X POST https://$FIREWALL_IP/api/?type=keygen -d "user=$USER&password=$PASSWORD")
+echo $API_KEY
