694c28014900647756f0bfc077f32218228e13ee
Tri Tran <tritran@Tris-MacBook-Air-3.local>
2025-01-16 20:02:30 -0500

    new and improved palo alto rules testing

--- palo_alto_test.sh
+++ palo_alto_test.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
 FIREWALL_IP=""
-read -s -p "Enter firewall IP: " FIREWALL_IP
+read -s -p "Enter firewall IP: \n" FIREWALL_IP
 
 USER=""
-read -s -p "Enter username: " USER
+read -s -p "Enter username: \n" USER
 
 PASSWORD=""
-read -s -p "Enter password: " PASSWORD
+read -s -p "Enter password: \n" PASSWORD
 
 RAW=$(curl -k -H "Content-Type: application/x-www-form-urlencoded" -X POST https://$FIREWALL_IP/api/?type=keygen -d "user=$USER&password=$PASSWORD")
 API_KEY=$(awk -v str="$RAW" 'BEGIN { split(str, parts, "<key>|</key>"); print parts[2] }')
-echo $API_KEY
+
+create_rule() {
+    local rule_name="$1"
+    local from_zone="$2"
+    local to_zone="$3"
+    local source_ip="$4"
+    local dest_ip="$5"
+    local service="$6"
+    local application="$7"
+    local action="$8"
+
+    curl -k -X POST "https://$FIREWALL_IP/api/ \
+    ?type=config&action=set&key=$API_KEY \
+    &xpath=/config/devices/entry/vsys/entry[@name='vsys1']/rulebase/security/rules \
+    &element=<entry name=$rule_name> \
+    <from><member>$from_zone</member></from> \
+    <to><member>$to_zone</member></to> \
+    <source><member>$source_ip</member></source> \
+    <destination><member>$dest_ip</member></destination> \
+    <service><member>$service</member></service> \
+    <application><member>$application</member></application> \
+    <action>$action</action></entry>"
+}
+
+create_rule "test" "any" "any" "any" "any" "tcp-80" "application-default" "allow"
