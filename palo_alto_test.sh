c12b84ee40270b3a6b5acf065eadd5459a1789f2
Tri Tran <chemandtech874@gmail.com>
2025-01-16 20:24:42 -0500

    new api rules work, now to add all rules

--- palo_alto_test.sh
+++ palo_alto_test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 FIREWALL_IP=""
-read -s -p "Enter firewall IP: \n" FIREWALL_IP
+read -s -p "Enter firewall IP: " FIREWALL_IP
+echo ""
 
 USER=""
-read -s -p "Enter username: \n" USER
+read -s -p "Enter username: " USER
+echo ""
 
 PASSWORD=""
-read -s -p "Enter password: \n" PASSWORD
+read -s -p "Enter password: " PASSWORD
+echo ""
 
 RAW=$(curl -k -H "Content-Type: application/x-www-form-urlencoded" -X POST https://$FIREWALL_IP/api/?type=keygen -d "user=$USER&password=$PASSWORD")
 API_KEY=$(awk -v str="$RAW" 'BEGIN { split(str, parts, "<key>|</key>"); print parts[2] }')
@@ -21,18 +24,22 @@ create_rule() {
     local service="$6"
     local application="$7"
     local action="$8"
-
-    curl -k -X POST "https://$FIREWALL_IP/api/ \
-    ?type=config&action=set&key=$API_KEY \
-    &xpath=/config/devices/entry/vsys/entry[@name='vsys1']/rulebase/security/rules \
-    &element=<entry name=$rule_name> \
-    <from><member>$from_zone</member></from> \
-    <to><member>$to_zone</member></to> \
-    <source><member>$source_ip</member></source> \
-    <destination><member>$dest_ip</member></destination> \
-    <service><member>$service</member></service> \
-    <application><member>$application</member></application> \
-    <action>$action</action></entry>"
+    
+    curl -k -X POST "https://$FIREWALL_IP/api/?type=config&action=set&key=$API_KEY" \
+        --data-urlencode "xpath=/config/devices/entry/vsys/entry[@name='vsys1']/rulebase/security/rules" \
+        --data-urlencode "element=<entry name='$rule_name'>
+            <from><member>$from_zone</member></from> 
+            <to><member>$to_zone</member></to> 
+            <source><member>$source_ip</member></source> 
+            <destination><member>$dest_ip</member></destination> 
+            <service><member>$service</member></service> 
+            <application><member>$application</member></application> 
+            <action>$action</action>
+        </entry>"
 }
 
-create_rule "test" "any" "any" "any" "any" "tcp-80" "application-default" "allow"
+create_rule "test" "any" "any" "any" "any" "service-http" "any" "allow"
+
+curl -k -X POST "https://$FIREWALL_IP/api/?type=commit&key=$API_KEY" \
+	--data-urlencode "cmd=<commit><description>blue4life</description></commit>"
+
