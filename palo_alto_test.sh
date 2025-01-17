08f4dcb9a99e13d96a6092c68765845c7b7b644f
Tri Tran <tritran@Tris-MacBook-Air-3.local>
2025-01-17 16:20:42 -0500

    parameterize rule fixes

--- palo_alto_test.sh
+++ palo_alto_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+fixes() {
+    change_rule_status "test" "no"
+}
+
 FIREWALL_IP=""
 read -s -p "Enter firewall IP: " FIREWALL_IP
 echo ""
@@ -38,7 +42,52 @@ create_rule() {
         </entry>"
 }
 
-create_rule "test" "any" "any" "any" "any" "service-http" "any" "allow"
+change_rule_status() {
+    local rule_name="$1"
+    local action="$2"
+
+    curl -k -X POST "https://$FIREWALL_IP/api/?type=config&action=set&key=$API_KEY" \
+    --data-urlencode "xpath=/config/devices/entry/vsys/entry[@name='vsys1']/rulebase/security/rules" \
+    --data-urlencode "/entry[@name=$rule_name]&element=<disabled>$action</disabled>"
+}
+
+inital() {
+    # All Win to DC
+    # TCP: 88,135,389,445,464,636,3268
+    # UDP: 53, 88,123,135,389,445,464,636
+
+    # All win out to dc
+    # UDP: 53
+
+    # DC out
+    # UDP: 53
+
+    # All win to CA
+    # TCP: 135
+    # UDP: 135
+
+    # from ansible to all win 
+    # TCP: 5985, 5986
+    # from all outside to all win
+    # TCP: 3389
+    # UDP: 3389
+    # From All Win to Wazuh and Graylog 
+    # TCP: 1514, 1515, 80, 443
+
+    create_rule "test" "any" "any" "any" "any" "service-http" "any" "allow"
+}
+
+if [ "$1" -eq "init" ]; then
+    initial()
+elif [ "$1" -eq "fix" ]; then
+    fixes()
+elif [ $# -gt 1 || $# -eq 0]; then
+    echo "only one parameter allowed"
+    exit
+else
+    echo "invalid parameter, use 'init' or 'fix'"
+    exit
+fi
 
 curl -k -X POST "https://$FIREWALL_IP/api/?type=commit&key=$API_KEY" \
 	--data-urlencode "cmd=<commit><description>blue4life</description></commit>"
