9d4b46c8649007e981cee929a2f410e8a0bd8c17
Leah Kvares <lk3053@g.rit.edu>
2026-01-19 22:57:33 -0500

    fix id_like error

--- tools/setup.sh
+++ tools/setup.sh
@@ -44,7 +44,7 @@ unset BLUETEAM_PASS BLUETEAM_PASS_CONFIRM
 if [ -f /etc/os-release ]; then
   . /etc/os-release
   OS_FAMILY=""
-  case "$ID_LIKE $ID" in
+  case "${ID_LIKE:-} $ID" in
     *alpine*)
       OS_FAMILY="Alpine"
       ;;
@@ -91,7 +91,7 @@ case "$OS_FAMILY" in
     usermod -aG sudo,adm,blueteam "${BLUETEAM_USER}"
     ;;
 esac
-
+b
 # add key to blueteam user
 # if [ -f "$SSH_PUBKEY_FILE" ]; then
 #   echo "[*] Installing SSH key"
@@ -116,20 +116,17 @@ case "$OS_FAMILY" in
   Debian)
     apt update -y
     apt install -y \
-      yara lsof vim curl openssl iptables \
-      snoopy lynis iptables-persistent jq unzip git || true
+      yara lsof vim curl openssl iptables snoopy lynis iptables-persistent jq unzip git || true
     ;;
   RedHat)
     dnf makecache -y
     dnf install -y \
-      yara lsof vim curl openssl iptables \
-      snoopy lynis jq unzip git || true
+      yara lsof vim curl openssl iptables snoopy lynis jq unzip git || true
     ;;
   Suse)
     zypper refresh
     zypper install -y \
-      yara lsof vim curl openssl iptables \
-      lynis jq unzip git || true
+      yara lsof vim curl openssl iptables lynis jq unzip git || true
     ;;
 esac
 
