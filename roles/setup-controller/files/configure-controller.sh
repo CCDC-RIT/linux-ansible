0e90a1b62d07ea3836695379954f62a8e73c0a13
Max49 <massimomarino49@gmail.com>
2026-03-11 00:46:37 -0400

    add comments and fix key

--- roles/setup-controller/files/configure-controller.sh
+++ roles/setup-controller/files/configure-controller.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+# Load os-release variables into memory
 if [ -f /etc/os-release ]; then
     . /etc/os-release
 else
@@ -17,28 +18,34 @@ for user_dir in /home/*; do
     fi
 done
 
+# create .ssh directory in user home for key
 if [[ ! -d /"home/$home_user/.ssh" ]]; then
     mkdir -p /"home/$home_user/.ssh"
     chown "$home_user:$home_user" /"home/$home_user/.ssh"
     chmod 700 /"home/$home_user/.ssh"
 fi
 
+# create key for blueteam user
 if [[ ! -f "/home/$home_user/.ssh/id_ed25519" ]]; then
-    ssh-keygen -t ed25519 -f "/home/$home_user/.ssh/id_ed25519" -N \"\" -q
+    ssh-keygen -t ed25519 -f "/home/$home_user/.ssh/id_ed25519" -N "" -q
     chown "$home_user:$home_user" /"home/$home_user/.ssh/id_ed25519"
     chmod 640 /"home/$home_user/.ssh/id_ed25519"
 fi
 
+# create backups directory
 mkdir -p /opt/backups
 
+# download yara and yara rules to controller
 curl -L https://github.com/VirusTotal/yara/archive/refs/tags/v4.5.4.tar.gz -o "/home/$home_user/linux-ansible/roles/setup-hosts-other/files/yara.tar.gz"
 curl -L https://github.com/CCDC-RIT/YaraRules/raw/refs/heads/main/Linux.zip -o "/home/$home_user/linux-ansible/roles/setup-hosts-other/files/Linux.zip"
 curl -L https://github.com/CCDC-RIT/YaraRules/raw/refs/heads/main/Multi.zip -o "/home/$home_user/linux-ansible/roles/setup-hosts-other/files/Multi.zip"
 curl -L https://github.com/YARAHQ/yara-forge/releases/latest/download/yara-forge-rules-full.zip -o "/home/$home_user/linux-ansible/roles/setup-hosts-other/files/yara-forge-rules-full.zip"
 chown -R "$home_user:$home_user" "/home/$home_user/linux-ansible/roles/setup-hosts-other/files/"
 
+# why did james think the docker packages were ok
 apt remove docker-compose-v2 -y
 
+# installing docker the right way (from the online docs)
 install -m 0755 -d /etc/apt/keyrings
 curl -o /etc/apt/keyrings/docker.asc https://download.docker.com/linux/ubuntu/gpg
 chmod 0644 /etc/apt/keyrings/docker.asc
@@ -50,20 +57,26 @@ apt-get update
 apt install docker-ce docker-compose-plugin -y
 systemctl restart docker
 
+# pull password manager container (hopefully we have access to the internet)
 docker pull ghcr.io/ccdc-rit/password-manager:latest
 docker save ghcr.io/ccdc-rit/password-manager:latest > "/home/$home_user/linux-ansible/roles/password-manager-server/files/password-manager-latest.tar"
 
+# ?
 gzip "/home/$home_user/linux-ansible/roles/password-manager-server/files/password-manager-latest.tar" --force
 
+# Download password manager docker compose file from repo
 curl -L https://raw.githubusercontent.com/CCDC-RIT/Password-Manager/refs/heads/main/docker-compose.yml -o "/home/$home_user/linux-ansible/roles/password-manager-server/files/docker-compose.yml"
 
+# Create password manager files dir if it doesn't already exist
 if [ ! -d "/home/$home_user/linux-ansible/roles/password-manager-client/files" ]; then
     mkdir -p "/home/$home_user/linux-ansible/roles/password-manager-client/files"
 fi
 
+# Download the linux client and the linux service for password manager client
 curl -L https://github.com/CCDC-RIT/Password-Manager/raw/refs/heads/main/client/linux -o "/home/$home_user/linux-ansible/roles/password-manager-client/files/ccdc-password-manager-client"
 curl -L https://raw.githubusercontent.com/CCDC-RIT/Password-Manager/refs/heads/main/client/linux.service  -o "/home/$home_user/linux-ansible/roles/password-manager-client/files/ccdc-password-manager-client.service"
 
+# permissions on the password manager files
 chown -R "$home_user:$home_user" "/home/$home_user/linux-ansible/roles/password-manager-server/files/"
 chown -R "$home_user:$home_user" "/home/$home_user/linux-ansible/roles/password-manager-client/files/"
 chmod 0774 "/home/$home_user/linux-ansible/roles/password-manager-client/files/ccdc-password-manager-client"
