29ef1cbe579fe5364c5e55be2673d1435dc52185
bcvillano <bvillano05@gmail.com>
2026-03-11 23:27:41 -0400

    Add -L to curl

--- roles/setup-hosts-other/files/falcosidekick-setup.sh
+++ roles/setup-hosts-other/files/falcosidekick-setup.sh
@@ -5,6 +5,6 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-curl https://github.com/falcosecurity/falcosidekick/releases/download/2.33.0/falcosidekick_2.33.0_linux_amd64.tar.gz -o falcosidekick.tar.gz
+curl -L https://github.com/falcosecurity/falcosidekick/releases/download/2.33.0/falcosidekick_2.33.0_linux_amd64.tar.gz -o falcosidekick.tar.gz
 tar -xzvf falcosidekick.tar.gz
 mv falcosidekick /usr/local/bin/falcosidekick
