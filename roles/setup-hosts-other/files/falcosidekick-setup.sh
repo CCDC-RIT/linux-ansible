285ad5c987e8ac1ec7bc2d7d093ba3ae21f475c6
bcvillano <bvillano05@gmail.com>
2026-03-11 23:56:09 -0400

    Fix falcosidekick owner and group

--- roles/setup-hosts-other/files/falcosidekick-setup.sh
+++ roles/setup-hosts-other/files/falcosidekick-setup.sh
@@ -8,3 +8,4 @@ fi
 curl -L https://github.com/falcosecurity/falcosidekick/releases/download/2.33.0/falcosidekick_2.33.0_linux_amd64.tar.gz -o falcosidekick.tar.gz
 tar -xzvf falcosidekick.tar.gz
 mv falcosidekick /usr/local/bin/falcosidekick
+chown root:root /usr/local/bin/falcosidekick
